d74faed12f02c809c08168cabe41fe02b1e58fcf
Phil Cable <pcable@wsu.edu>
2018-09-18 11:18:29 -0700

    Output the give now link markup if the necessary meta is available

--- parts/single-layout.php
+++ parts/single-layout.php
@@ -6,6 +6,19 @@
 
 			<?php get_template_part( 'articles/post', get_post_type() ) ?>
 
+			<?php
+			$link_label = get_post_meta( get_the_ID(), '_wsuwp_cvm_advance_post_give_label', true );
+			$link_url = get_post_meta( get_the_ID(), '_wsuwp_cvm_advance_post_give_url', true );
+
+			if ( $link_label && $link_url ) {
+				?>
+				<div class="cvm-give-now-link">
+					<a href="<?php echo esc_url( $link_url ); ?>"><?php echo esc_html( $link_label ); ?></a>
+				</div>
+				<?php
+			}
+			?>
+
 		<?php endwhile; ?>
 
 	</div><!--/column-->
