541260d3f10143d7c39e0e6eb4f3c3a1cf16dfbd
danialbleile <danial.bleile@wsu.edu>
2018-10-23 15:42:48 -0700

    Add featured title

--- parts/blocks/featured.php
+++ parts/blocks/featured.php
@@ -7,11 +7,11 @@ $featured = cmv_get_featured_posts();
 	<h2>Features</h2>
 	<div class="featured-set-wrapper">
 		<?php foreach ( $featured as $feature ) : ?>
-		<article>
-			<h1><?php echo esc_html( $feature['title'] ); ?></h1>
+		<article class="featured-issue-article">
 			<figure style="background-image:url(<?php echo esc_url( $feature['img_src'] ); ?>)">
 				<img src="<?php echo esc_url( $feature['img_src'] ); ?>" alt="<?php echo esc_html( $feature['img_alt'] ); ?>"/>
 			</figure>
+			<h2><?php echo esc_html( $feature['title'] ); ?></h2>
 			<p><?php echo wp_kses_post( $feature['excerpt'] ); ?></p>
 			<a class="article-link" href="<?php echo esc_url( $feature['link'] ); ?>">Learn more about <?php echo esc_html( $feature['title'] ); ?></a>
 		</article>
